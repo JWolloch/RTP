916ed9a9f6d4f89eb49863cb5c6216e2cf7cf402
yoni <yoni@justvoltify.com>
2025-06-22 15:18:27 +0300

    Update presentation.tex to clarify definitions and applications of uncertain-LP and X-rays, and modify config.py to adjust optimization parameters including solution method and biological dose settings for improved radiotherapy treatment planning.

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 from enum import IntEnum
+import numpy as np
 
 class SolutionMethod(IntEnum):
     PRIMAL_SIMPLEX = 0
     PRIMAL_DUAL_SIMPLEX = 1
     BARRIER = 2
-    CONCURRENT = 3
 
 @dataclass
 class GammaParameters:
@@ -25,16 +25,17 @@ class ProjectionParameters:
 class OptimizationParameters:
     debug: bool = False
     debug_n: int = 1000
-    solution_method: SolutionMethod = SolutionMethod.BARRIER # Run with [Primal-simplex, Primal-dual-simplex, Barrier]
+    solution_method: SolutionMethod = SolutionMethod.PRIMAL_DUAL_SIMPLEX # Run with [Primal-simplex, Primal-dual-simplex, Barrier] barrier if it takes too long
     row_generation: bool = True
-    n_most_violated_constraints: int = 1 # Run with [1, 2, 5, 10]
+    n_most_violated_constraints: int = 5 # Run with [5, 10] priority 2
+    max_constraint_addition: int = np.inf #or 2000 priority 1
     max_row_generation_iterations: int = 100
     N: int = 2
     lam: float = 0.5
-    mu_F: float = 1.1
-    d_bar_F: float = 0.95
-    d_bar: float = 0.9 * d_bar_F * N
-    d_bar_F_organ_1: float = 0.95
-    d_bar_organ_1: float = 0.9 * d_bar_F_organ_1 * N
-    d_bar_F_organ_2: float = 0.95
-    d_bar_organ_2: float = 0.9 * d_bar_F_organ_2 * N
+    mu_F: float = 1.3 # 1.1, 1.15, 1.2,... priority 3
+    d_bar_organ_1: float = 60
+    d_bar_F_organ_1: float = d_bar_organ_1/N * 1.1
+    d_bar_organ_2: float = 50
+    d_bar_F_organ_2: float = d_bar_organ_2/N * 1.1
+    d_bar_organ_3: float = 60
+    d_bar_F_organ_3: float = d_bar_organ_2/N * 1.1
